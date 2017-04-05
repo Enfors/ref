3287e4dfb29597eabd47dc17d6a7a188bcb0a907
Christer Enfors <christer.enfors@gmail.com>
2017-04-05 13:56:19 +0200

    Add section about decorators.

--- python-ref/python-ref.py
+++ python-ref/python-ref.py
@@ -27,3 +27,35 @@
 
 import pdb
 pdb.set_trace()                 # Sets a breakpoint here.
+
+
+####################################################################
+#
+# DECORATORS
+
+# This is the actual decorator.
+def my_decorator(original_function):
+    def new_function(*args, **kwargs):
+        print("Preparing to call the function.")
+        print("args: " + str(args))
+        print("kwargs: " + str(kwargs))
+        original_function(*args, **kwargs)
+        print("Called the original function.")
+
+    return new_function
+
+
+# This is an annotation. It prevents hello from being called directly.
+# Instead, the decorator (my_decorator) is called, which returns a
+# function. THAT function is called, instead of hello.
+@my_decorator
+def hello(target_name=None):
+    if target_name:
+        print("Hello, " + target_name + "!")
+    else:
+        print("Hello, world!")
+
+
+hello2()
+hello2("foo")
+                                                                    
